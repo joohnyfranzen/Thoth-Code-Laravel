bb40656ab2ac0a1b6c2ce171dfdb0a5b2c0ee3b2
Jonathan <joohny_2014@hotmail.com>
2022-09-24 16:14:47 -0300

    Login Route

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('user', [AuthController::class, 'store'])->name('user');
+Route::post('login', [AuthController::class, 'login'])->name('login');
