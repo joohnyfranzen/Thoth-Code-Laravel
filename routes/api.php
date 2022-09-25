4093a201acfd5655e1d4c5700047dbd07c12a773
Jonathan <joohny_2014@hotmail.com>
2022-09-25 15:21:21 -0300

    Routes edited and add Post endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('user', [AuthController::class, 'store'])->name('user');
+
+// Login Route
 Route::post('login', [AuthController::class, 'login'])->name('login');
+
+// Register Route
+Route::post('user', [AuthController::class, 'store'])->name('user');
+
+// Post unprotected Routes
+Route::get('post', [PostControlller::class, 'index'])->name('post');
+Route::get('post/:id', [PostControlller::class, 'show'])->name('post');
+
+// Protected Routes
+Route::middleware('auth:sanctum')->group(function() {
+
+    // Post Resource Routes
+    Route::Resource('post', PostController::class)->except('index', 'show', 'create', 'edit');
+
+    
+});
