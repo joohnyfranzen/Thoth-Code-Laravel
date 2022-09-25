918c2625196ba8396eb259be2191c34d3ee4fdb8
Jonathan <joohny_2014@hotmail.com>
2022-09-25 15:37:57 -0300

    Function store and index created

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,37 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Constructor
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    // Index 10 posts
     public function index()
     {
-        //
+        $posts = $this->post->paginate('10');
+
+        return response()->json($posts, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    // Create a new post
+    public function store(PostRequest $request)
     {
-        //
+        $data = $request->all();
+
+        if(!$data.['title'] || !$data.['slug'] || $data.['content'])
+        {
+            return response()->json('All field must be filleds');
+        }
+
+        try{
+            $post = $this->post->create($data);
+
+            return response()->json($post);
+
+        } catch (\Exception $e) {
+            return response()->json($e);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
