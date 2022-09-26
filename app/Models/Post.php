39b6d7f638c3a4c124aff0b067ccb22936f4493c
Jonathan <joohny_2014@hotmail.com>
2022-09-26 13:32:14 -0300

    Relation and fillable Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
